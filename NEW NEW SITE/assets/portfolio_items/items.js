1977819c0a5214699c7a88a8a09b0a63f62faff4
max-volkan <sunlitlife.fy@gmail.com>
2024-08-02 11:11:40 -0700

    booking button

--- NEW NEW SITE/assets/portfolio_items/items.js
+++ NEW NEW SITE/assets/portfolio_items/items.js
@@ -1,38 +1,34 @@
 const portfolioItems = [
   {
-    "filename": "Artboard 1-Enhanced-SR.jpg",
-    "category": "design"
-  },
-  {
-    "filename": "IMG_3121.jpg",
-    "category": "design"
-  },
-  {
-    "filename": "IMG_7462.jpg",
-    "category": "design"
-  },
-  {
-    "filename": "DSC01683.jpg",
-    "category": "photography"
-  },
-  {
-    "filename": "DSC02713.jpg",
-    "category": "photography"
-  },
-  {
-    "filename": "DSC02964.jpg",
-    "category": "photography"
-  },
-  {
-    "filename": "IMG_3780.jpg",
-    "category": "photography"
-  },
-  {
     "filename": "DSC01510.jpg",
-    "category": "retouch"
+    "category": "1retouch"
   },
   {
     "filename": "DSC01939.jpg",
-    "category": "retouch"
+    "category": "1retouch"
+  },
+  {
+    "filename": "DSC01683.jpg",
+    "category": "2photography"
+  },
+  {
+    "filename": "DSC02713.jpg",
+    "category": "2photography"
+  },
+  {
+    "filename": "DSC02964.jpg",
+    "category": "2photography"
+  },
+  {
+    "filename": "IMG_3780.jpg",
+    "category": "2photography"
+  },
+  {
+    "filename": "Artboard 1-Enhanced-SR.jpg",
+    "category": "3design"
+  },
+  {
+    "filename": "IMG_7462.jpg",
+    "category": "3design"
   }
 ];
